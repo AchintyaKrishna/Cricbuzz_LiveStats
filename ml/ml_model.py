6a213dd902ac1186de5bd58ffd8aa81039f386f1
Achintya Krishna <153733087+AchintyaKrishna@users.noreply.github.com>
2026-04-23 22:31:43 +0530

    Update ml_model.py

--- ml/ml_model.py
+++ ml/ml_model.py
@@ -2,7 +2,6 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 def train_model():
-    # FIX: ensure 2 classes exist
     X = np.array([
         [180,170],
         [150,160],
@@ -10,7 +9,7 @@ def train_model():
         [140,150]
     ])
 
-    y = np.array([1,0,1,0])  # MUST HAVE BOTH CLASSES
+    y = np.array([1,0,1,0])
 
     model = LogisticRegression()
     model.fit(X,y)
